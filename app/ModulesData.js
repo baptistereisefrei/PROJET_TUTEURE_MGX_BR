afb2ce867112e67a6beffcf4c0915b6216ed1877
Margaux <margaux.soule28@gmail.com>
2024-01-24 15:36:15 +0100

    All new front end, page detail done

--- app/ModulesData.js
+++ app/ModulesData.js
@@ -6,20 +6,24 @@ const modulesData = [
       heure_debut: "16",
       heure_fin: "18",
       description: "Venez découvrir le fitness avec le club de l'école !",
-      photo: "/img/module1.jpg",
+      photo: "/img/img4-1.jpg",
       category: "sport",
-      slug: "fitness"
+      slug: "fitness",
+      imgBeginning:"/img/img4-2.jpg",
+      imgEnd: "/img/img4-3.jpg"
     },
     {
       title: "Séance natation",
-      detail: "Découvrez notre cours de natation captivant, fruit d'un partenariat exclusif avec une école renommée. Plongez dans une expérience aquatique enrichissante, alliant expertise pédagogique et plaisir aquatique. Ce programme unique offre une approche holistique de l'apprentissage de la natation, adaptée à tous les niveaux. Encadrés par des instructeurs qualifiés et passionnés, les élèves bénéficient d'une progression rapide et sécurisée. Les installations modernes de la piscine offrent un cadre idéal pour développer la confiance dans l'eau et perfectionner les techniques de nage. Relevez le défi aquatique avec notre cours, alliant éducation et divertissement pour former des nageurs confiants et compétents.",
+      detail: "La natation, un ballet gracieux au cœur de l'élément liquide, transcende les frontières du sport pour devenir une danse aquatique empreinte de grâce et d'endurance. Plonger dans l'eau cristalline, c'est fusionner avec un univers fluide où chaque mouvement devient une poésie en mouvement. Les nageurs, telles des créatures aquatiques, tracent des sillons d'ondes harmonieuses à la surface de la piscine. Les bras s'étirent et se replient en une cadence rythmique, propulsant le corps avec une fluidité presque artistique. Les pieds, fins comme des palmes, battent en harmonie, propulsant le nageur vers l'avant avec une grâce naturelle. La natation n'est pas simplement un exercice physique, mais une communion avec l'eau qui apaise et revitalise. Chaque brassée devient une méditation, chaque respiration un moment de connexion avec soi-même. Les compétitions deviennent des ballets aquatiques où les nageurs rivalisent non seulement pour la victoire, mais aussi pour atteindre une symbiose parfaite avec l'élément liquide. Que ce soit en eau libre, dans les piscines olympiques ou les mers tumultueuses, la natation transcende les limites terrestres, offrant une expérience sensorielle unique. Elle incarne la force, l'élégance et la persévérance, propulsant les nageurs vers de nouveaux horizons tout en rappelant la beauté simple de flotter, de glisser et de s'immerger dans l'océan apaisant de la natation.",
       date: "29/01/2024",
       heure_debut: "16",
       heure_fin: "18",
       description: "Venez nager avec le club natation !",
-      photo: "/img/module2.jpg",
+      photo: "/img/img3-1.jpg",
       category: "sport",
-      slug: "natation"
+      slug: "natation",
+      imgBeginning:"/img/img3-2.jpg",
+      imgEnd: "/img/img3-3.jpg"
     },
     {
       title: "Cours particulier maths",
@@ -28,9 +32,11 @@ const modulesData = [
       heure_debut: "18",
       heure_fin: "20",
       description: "Viens renforcer tes connaissances.",
-      photo: "/img/module3.jpg",
+      photo: "/img/img2-1.jpg",
       category: "cours",
-      slug: "maths"
+      slug: "maths",
+      imgBeginning:"/img/img2-2.jpg",
+      imgEnd: "/img/img2-3.jpg"
     },
     {
       title: "Cours particulier physique",
@@ -39,9 +45,11 @@ const modulesData = [
       heure_debut: "18",
       heure_fin: "20",
       description: "Viens renforcer tes connaissances en physique.",
-      photo: "/img/module4.jpg",
+      photo: "/img/img1-1.jpg",
       category: "cours",
-      slug: "physique"
+      slug: "physique",
+      imgBeginning:"/img/img1-2.jpg",
+      imgEnd: "/img/img1-3.jpg"
     },
     {
       title: "Séance d'équitation",
@@ -50,9 +58,11 @@ const modulesData = [
       heure_debut: "17",
       heure_fin: "18",
       description: "Viens essayer le saut d'obstacle.",
-      photo: "/img/module5.jpg",
+      photo: "/img/img5-1.jpg",
       category: "sport",
-      slug: "equitation"
+      slug: "equitation",
+      imgBeginning:"/img/img5-2.jpg",
+      imgEnd: "/img/img5-3.jpg"
     },
   ];
 
--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,7 +1,9 @@
 'use client'
- 
+import styles from "../styles.module.css";
 import { usePathname } from 'next/navigation'
 import modulesData from '../ModulesData';
+import Link from "next/link";
+import Image from 'next/image';
 
 export default function Page() {
   const pathName = usePathname();
@@ -14,7 +16,38 @@ export default function Page() {
  
   return (
     <div>
-      {module.title}
+        <body className={styles.bodyStyle}>
+            <div className={styles.header + ' ' + styles.headerSlug}>
+                <Link href={'/'} className={styles.h1Style + ' ' + styles.backHomeButton}>Retour à l'accueil</Link>
+                <h1 className={styles.h1Style}>Évènements</h1>
+            </div>
+            <div className={styles.detailsEvent}>
+                <div className={styles.titleContainerEvent}>
+                    <Image
+                        src={module.imgBeginning}
+                        width={700}
+                        height={400}
+                        className={styles.imgBeginning}
+                        alt="Image d'illustration"
+                    />
+                    <div className={styles.titleOverlayEvent}>
+                        <h2 className={styles.titleEvent}>{module.title}</h2>
+                    </div>
+                </div>
+                <div className={styles.detailContainerEvent}>
+                    <p className={styles.descriptionEvent}>{module.detail}</p>
+                </div>
+                <div className={styles.imgContainerEvent}>
+                    <Image
+                        src={module.imgEnd}
+                        width={700}
+                        height={400}
+                        className={styles.imgEnd}
+                        alt="Image d'illustration"
+                    />
+                </div>
+            </div>
+        </body>
     </div>
   )
 }
